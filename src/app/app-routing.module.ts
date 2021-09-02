0b553516a42553a1e81aa6aab7f4f4317600a880
henrik <henrik.sandhall@se.experis.com>
2021-09-02 10:49:05 +0200

    feat - added components for pages and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { ProfilePageComponent } from './components/profile-page/profile-page.component';
+import { ProjectPageComponent } from './components/project-page/project-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component: MainPageComponent,
+  },
+  {
+    path:"profile",
+    component: ProfilePageComponent,
+  },
+  {
+    path:"project",
+    component: ProjectPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { ProfilePageComponent } from './components/profile-page/profile-page.component';
+import { ProjectPageComponent } from './components/project-page/project-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    ProfilePageComponent,
+    ProjectPageComponent
   ],
   imports: [
     BrowserModule,
